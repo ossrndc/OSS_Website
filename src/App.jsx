9effc670e2448950d43e2dc65da6c29bd1cbc945
shabd-sahay <SHABDSAHAY.MDB@GMAIL.COM>
2023-06-06 01:17:33 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,99 @@
+import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
+import Events from './components/Events';
 import Domains from './components/Domains';
+import one from './assets/events/1.jpg';
+import two from './assets/events/2.jpg';
+import three from './assets/events/3.png';
+import four from './assets/events/4.jpg';
+import five from './assets/events/5.png';
+import six from './assets/events/6.jpg';
+import seven from './assets/events/7.png';
+import nine from './assets/events/9.jpg';
+import ten from './assets/events/10.jpg';
+import eleven from './assets/events/11.png';
 
 export default function App() {
+  const events = [
+    {
+      id: '1',
+      type:"workshop",
+      name:"Security and Linux",
+      desc:"",
+      image: one,
+    },
+    {
+      id: '2',
+      type:"Contest",
+      name:"Code-O-Fiesta'16",
+      desc:"",
+      image: two,
+    },
+    {
+      id: '3',
+      type:"Contest",
+      name:"The Triwizard Tournament",
+      desc:"",
+      image: three,
+    },
+    {
+      id: '4',
+      type:"Contest",
+      name:"Codio_Fiesta'17",
+      desc:"",
+      image: four,
+    },
+    {
+      id: '5',
+      type:"Workshop",
+      name:"Angular 5/Java Training",
+      desc:"",
+      image: five,
+    },
+    {
+      id: '6',
+      type:"Workshop",
+      name:"Cypher",
+      desc:"",
+      image: six,
+    },
+    {
+      id: '7',
+      type:"Workshop",
+      name:"Cypher'20",
+      desc:"",
+      image: seven,
+    },
+    {
+      id: '9',
+      type:"Contest",
+      name:"Code Kaze",
+      desc:"",
+      image: nine,
+    },
+    {
+      id: '10',
+      type:"Contest",
+      name:"Code_O_Fiesta 5.0",
+      desc:"",
+      image: ten,
+    },
+    {
+      id: '11',
+      type:"Workshop",
+      name:"Node Jam",
+      desc:"",
+      image: eleven,
+    },
+  ];
+
   return (
     <div className="App">
       <Home />
       <About />
+      <Events events={events} />
       <Domains />
     </div>
   );
---
+++ src/components/Events/index.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+const Event = ({ events }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((currentIndex + 1) % events.length);
+  };
+
+  const handlePrevious = () => {
+    setCurrentIndex((currentIndex - 1 + events.length) % events.length);
+  };
+
+  return (
+    <div className="event-container">
+      <div className="view-all-button">View All</div>
+      <h2 className="event-title">Our Events</h2>
+      <p className='event-sub-title'>Event Wrapup and Highlights</p>
+      <div className="slider-container">
+        <div className="slider-track" style={{ transform: `translateX(-${currentIndex * 33.33}%)` }}>
+          {events.map((event, index) => (
+            <div className={`card-container ${index === currentIndex ? 'active' : ''}`} key={event.id}>
+              <div className="card-image-container">
+                <img className="card-image" src={event.image} alt={`Event ${event.id}`} />
+              </div>
+              <div className="card-content">
+                <p className="event-type">{event.type}</p>
+                <p className="event-name">{event.name}</p>
+                <p className="event-desc">{event.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="radio-buttons">
+          {events.map((event, index) => (
+            <label key={event.id}>
+              <input
+                type="radio"
+                name="event-radio"
+                value={index}
+                checked={index === currentIndex}
+                onChange={() => setCurrentIndex(index)}
+              />
+              <span className="radio-label" />
+            </label>
+          ))}
+      </div>
+      <div className="slider-buttons">
+        <button className="slider-btn prev " onClick={handlePrevious}>
+          Previous
+        </button>
+        <button className="slider-btn next " onClick={handleNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Event;
